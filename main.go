23134d9c717c42c5cf2ebc8fa13e241019aa7459
Antony Holmes <antony@antonyholmes.dev>
2025-02-05 11:05:24 -0500

    fix: Allow all hosts

--- main.go
+++ main.go
@@ -429,6 +429,6 @@ func main() {
 		})
 	})
 
-	r.Run() //"localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:8080") //"localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
